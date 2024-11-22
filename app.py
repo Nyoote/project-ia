e18b64048255d36f79a840980838a14992bdc271
Nyoote <faustinecharrier@gmail.com>
2024-11-22 11:31:12 +0100

    feat: implement ollama + clean project

---
+++ app.py
@@ -0,0 +1,57 @@
+import torch
+import os
+import argparse
+import ollama
+from localrag import ollama_chat
+from upload import upload_text
+
+PINK = '\033[95m'
+CYAN = '\033[96m'
+YELLOW = '\033[93m'
+GREEN = '\033[92m'
+RESET_COLOR = '\033[0m'
+
+if __name__ == "__main__":
+
+    print(GREEN + "Parsing command-line arguments..." + RESET_COLOR)
+    parser = argparse.ArgumentParser(description="Ollama Chat")
+    parser.add_argument("--model", default="llama3.2:1b", help="Ollama model to use (default: llama3.2:1b)")
+    parser.add_argument("--no-rag", action="store_false", dest="rag", help="Start script without localrag")
+    args = parser.parse_args()
+
+    vault_content = []
+    vault_embeddings = []
+
+    if args.rag :
+        upload_text()
+        if os.path.exists("vault.txt"):
+            print(GREEN + "Loading vault content..." + RESET_COLOR)
+            with open("vault.txt", "r", encoding='utf-8') as vault_file:
+                vault_content = vault_file.readlines()
+            print(GREEN + "Generating embeddings for the vault content..." + RESET_COLOR)
+            for content in vault_content:
+                response = ollama.embeddings(model='mxbai-embed-large', prompt=content)
+                vault_embeddings.append(response["embedding"])
+            print(GREEN + "Converting embeddings to tensor..." + RESET_COLOR)
+        else:
+            print(PINK + "No vault to embed, pls try again." + RESET_COLOR)
+    else :
+        print(PINK + "No localrag" + RESET_COLOR)
+
+
+    vault_embeddings_tensor = torch.tensor(vault_embeddings) 
+
+    print(GREEN + "Starting conversation loop..." + RESET_COLOR)
+    conversation_history = []
+    system_message = "You are a helpful assistant that is an expert at extracting the most useful information from a given text. Also bring in extra relevant infromation to the user query from outside the given context."
+
+    while True:
+        if args.rag :
+            user_input = input(YELLOW + "Ask a question about volley-ball (or type 'quit' to exit): " + RESET_COLOR)
+        else:
+            user_input = input(YELLOW + "Ask a question (or type 'quit' to exit): " + RESET_COLOR)
+        if user_input.lower() == 'quit':
+            break
+        
+        response = ollama_chat(user_input, system_message, vault_embeddings_tensor, vault_content, args.model, conversation_history)
+        print(GREEN + "Response: \n\n" + response + RESET_COLOR)
--- upload.py
+++ upload.py
@@ -1,15 +1,19 @@
-import PyPDF2
 import requests
+import PyPDF2
 from io import BytesIO
-import json
-import argparse
-from openai import OpenAI
+
+PINK = '\033[95m'
+CYAN = '\033[96m'
+YELLOW = '\033[93m'
+GREEN = '\033[92m'
+RESET_COLOR = '\033[0m'
 
 def get_pdf_from_drive(drive_url):
     try:
         file_id = drive_url.split("/d/")[1].split("/")[0]
     except IndexError:
-        raise ValueError("Le lien Google Drive est invalide.")
+        print(PINK + "Google Drive link is invalid" + RESET_COLOR)
+        raise ValueError("Google Drive link is invalid")
 
     download_url = f"https://drive.google.com/uc?id={file_id}&export=download"
     response = requests.get(download_url)
@@ -17,10 +21,10 @@ def get_pdf_from_drive(drive_url):
     if response.status_code == 200:
         return BytesIO(response.content)
     else:
-        raise Exception(f"Échec du téléchargement. Code HTTP : {response.status_code}")
-
+        print(PINK + "Failed Download" + RESET_COLOR)
+        raise Exception("Failed Download")
+    
 def convert_pdf_to_text(file_path):
-    extracted_text = ""
     try:
         reader = PyPDF2.PdfReader(file_path)
         with open('vault.txt', 'w', encoding='utf-8') as txt_file:
@@ -28,42 +32,16 @@ def convert_pdf_to_text(file_path):
                 text = page.extract_text()
                 if text:
                     txt_file.write(text + "\n")
-                    extracted_text += text + "\n"
-        print(f"Le texte a été extrait et enregistré dans : vault.txt")
-        return extracted_text
+        print(GREEN + "Text has been created and saved in vault.txt" + RESET_COLOR)
     except Exception as e:
-        raise Exception(f"Erreur lors de l'extraction du texte : {e}")
+        print(PINK + "Error while extracting text" + RESET_COLOR)
+        raise Exception(f"Error while extracting text : {e}")
 
-if __name__ == "__main__":
-    drive_url = "https://drive.google.com/file/d/1aP0TDiB5JuABGo8VgNTryQS7exlWxWwu/view?usp=sharing"
-    ollama_api_url = "http://localhost:11434/v1"
-    
+def upload_text():
+    drive_url = "https://drive.google.com/file/d/1HpRnlEpXu0TK51V0uvgeCkxwHAw762IN/view?usp=sharing"
     try:
         file_path = get_pdf_from_drive(drive_url)
-        
-        extracted_text = convert_pdf_to_text(file_path) 
 
-        parser = argparse.ArgumentParser(description="Ollama Chat")
-        parser.add_argument("--model", default="llama3.2:1b", help="Ollama model to use (default: llama3.2:1b)")
-        args = parser.parse_args()
-
-    
-        print("Starting chat system. Type 'quit' to exit.")
-        conversation_history = [] 
-
-        vault_embeddings_tensor, vault_content = initialize_rag()
-        print("Starting chat system. Type 'quit' to exit.")
-        conversation_history = []
-        system_message = "You are a helpful assistant that is an expert at extracting the most useful information from a given text. Also bring in extra relevant information to the user query from outside the given context."
-
-        user_input = "No questions for now"
-        print("\nTraitement de votre question...\n")
-        response = ollama_chat(user_input, system_message, 
-                            vault_embeddings_tensor,
-                            extracted_text, args.model, 
-                            conversation_history)
-        print("Réponse:\n", response)
-        
+        convert_pdf_to_text(file_path)
     except Exception as err:
-        print(f"Erreur : {err}")
-
+        print(PINK + f"Error : {err}")
